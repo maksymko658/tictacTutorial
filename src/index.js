07e8b620ecfafe5e616af2e397d0a7baa4029851
maksymko658 <Jmaksy@ukr.net>
2017-10-12 00:29:39 +0300

    handleClick

--- src/index.js
+++ src/index.js
@@ -11,16 +11,32 @@ constructor(props) {
   }
   render() {
     return (
-      <button className="square" onClick={() => alert('click')}>
-        {this.state.value}
+      <button className="square"
+       onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
-
+//==============================================
 class Board extends React.Component {
+constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+ handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]}
+    onClick={() => this.handleClick(i)}
+     />;
   }
 
   render() {
@@ -48,7 +64,7 @@ class Board extends React.Component {
     );
   }
 }
-
+//=========================================
 class Game extends React.Component {
   render() {
     return (
